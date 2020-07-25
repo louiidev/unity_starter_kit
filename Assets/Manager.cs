9a599c93fbe4784837f60be952a78e74e4edeaf5
Louis Johnson <louisgjohnson@gmail.com>
2020-07-25 16:59:16 +1000

    first commit

---
+++ Assets/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static readonly System.Lazy<T> LazyInstance = new System.Lazy<T>(CreateSingleton);
+
+    public static T Instance => LazyInstance.Value;
+
+    private static T CreateSingleton()
+    {
+        var ownerObject = new GameObject($"{typeof(T).Name} (singleton)");
+        var instance = ownerObject.AddComponent<T>();
+        DontDestroyOnLoad(ownerObject);
+        return instance;
+    }
+}
+
+
+public class Manager : Singleton<Manager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
